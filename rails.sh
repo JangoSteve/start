ce3d9cf39b15081a0f293923c173b2b7b7274adc
Steve Schwartz <steve@alfajango.com>
2013-04-10 17:20:22 -0400

    Added support for rack and npm apps, as well as detecting Bundler to bash start function.

--- rails.sh
+++ rails.sh
@@ -1,17 +1,35 @@
 start(){
+  local foreman=false
   if [ -f ./Procfile.dev ];
   then
     echo "Procfile.dev detected"
-    echo "foreman start -f Procfile.dev"
-    foreman start -f Procfile.dev
+    local command="foreman start -f Procfile.dev -p 3000"
+    foreman=true
   elif [ -f ./Procfile ];
   then
     echo "Procfile detected"
-    echo "foreman start"
-    foreman start
+    local command="foreman start -p 3000"
+    foreman=true
+  elif [ -f ./config/boot.rb ]
+  then
+    echo "No Procfiles detected, falling back to rails"
+    local command="rails server"
+  elif [ -f ./config.ru ]
+  then
+    echo "Rack app detected"
+    local command="rackup config.ru"
+  elif [ -f ./package.json ]
+  then
+    echo "NPM app detected"
+    local command="npm start"
   else
     echo "No Procfiles detected, falling back to rails"
-    echo "rails server"
-    rails server
+    local command="rails server"
   fi
+  if [[ -f Gemfile && ! $foreman ]];
+  then
+    command="bundle exec $command"
+  fi
+  echo "> $command"
+  $command
 }
